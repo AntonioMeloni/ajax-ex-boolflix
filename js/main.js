d5bc9141de709dd16cbc69a48bee08e03942ab89
Antonio <ameloni9@gmail.com>
2020-03-20 17:18:33 +0100

    Inserito l'avvio ricerca tramite il tasto enter e aggiunta flags per la lingua

--- js/main.js
+++ js/main.js
@@ -2,62 +2,73 @@ $(document).ready(function () {
     var source = $("#card-template").html();
     var template = Handlebars.compile(source);
 
-    $(".btn-search").click(function(){
-        $('.card').remove();
-		var searchText = $(".search-input").val();
-        var apiBaseUrl = 'https://api.themoviedb.org/3';
-        var imgBaseUrl = 'https://image.tmdb.org/t/p/w185';
+    $(".btn-search").click(creaCard);
+    $('.search-input').keypress(function (event) {
+        if (event.key == 'Enter') {
+            creaCard();
+        }
+    })
 
-       $.ajax({
-           url: apiBaseUrl + '/search/movie',
-           data: {
-                   api_key: '7780c6a90a66dfa3e0b69e733e848d17',
-                   query: searchText,
-                   language: 'it-IT'
-                 },
-           method: 'GET',
-           success: function (data) {
 
-               var films = data.results;
-               console.log(films);
-               for (var i = 0; i < films.length; i++) {
-                   var film = films[i];
+        function creaCard() {
+            $('.card').remove();
+            var searchText = $(".search-input").val();
+            var apiBaseUrl = 'https://api.themoviedb.org/3';
+            var imgBaseUrl = 'https://image.tmdb.org/t/p/w185';
 
-                   var stars = '';
-                   var stellaPiena = '<i class="fas fa-star"></i>';
-                   var stellaVuota = '<i class="far fa-star"></i>';
-                   var nStelle = Math.ceil(film.vote_average/2);
-                   for (var x = 0; x < 5; x++) {
+           $.ajax({
+               url: apiBaseUrl + '/search/movie',
+               data: {
+                       api_key: '7780c6a90a66dfa3e0b69e733e848d17',
+                       query: searchText,
+                       language: 'it-IT'
+                     },
+               method: 'GET',
+               success: function (data) {
 
-                       if(x < nStelle){
-                           stars = stars + stellaPiena;
+                   var films = data.results;
+                   console.log(films);
+                   for (var i = 0; i < films.length; i++) {
+                       var film = films[i];
+
+                       var stars = '';
+                       var stellaPiena = '<i class="fas fa-star"></i>';
+                       var stellaVuota = '<i class="far fa-star"></i>';
+                       var nStelle = Math.ceil(film.vote_average/2);
+                       for (var x = 0; x < 5; x++) {
+
+                           if(x < nStelle){
+                               stars = stars + stellaPiena;
+                           }
+                           else {
+                               stars = stars + stellaVuota;
+                           }
                        }
-                       else {
-                           stars = stars + stellaVuota;
+
+                       var datiFilm = {
+                           cover: imgBaseUrl + film.poster_path,
+                           titolo: film.title,
+                           titoloOr: film.original_title,
+                           trama: film.overview,
+                           rate: nStelle,
+                           linguaOr:film.original_language,
+                           stelle: stars
                        }
+                       if (datiFilm.linguaOr == 'en') {
+                           datiFilm.linguaOr = 'gb';
+                       }
+
+                       var filmCard = template(datiFilm);
+                       $('.card-container').append(filmCard);
+
                    }
-
-                   var datiFilm = {
-                       cover: imgBaseUrl + film.poster_path,
-                       titolo: film.title,
-                       titoloOr: film.original_title,
-                       trama: film.overview,
-                       rate: nStelle,
-                       linguaOr:film.original_language,
-                       stelle: stars
-                   }
-
-
-                   var filmCard = template(datiFilm);
-                   $('.card-container').append(filmCard);
+               },
+               error: function (err) {
 
                }
-           },
-           error: function (err) {
+           })
+    };
 
-           }
-       })
-	});
 
 
 
