bed7f43d689e56ab51da0a3ddf43d3a24b25f6f2
Antonio <ameloni9@gmail.com>
2020-03-20 18:09:50 +0100

    Aggiunta la valutazione in stelle tramite funzione

--- js/main.js
+++ js/main.js
@@ -2,74 +2,85 @@ $(document).ready(function () {
     var source = $("#card-template").html();
     var template = Handlebars.compile(source);
 
-    $(".btn-search").click(creaCard);
+    $(".btn-search").click(function () {
+        $('.card').remove();
+        creaCard();
+    });
+
     $('.search-input').keypress(function (event) {
         if (event.key == 'Enter') {
+            $('.card').remove();
             creaCard();
         }
     })
 
-
-        function creaCard() {
-            $('.card').remove();
             var searchText = $(".search-input").val();
             var apiBaseUrl = 'https://api.themoviedb.org/3';
             var imgBaseUrl = 'https://image.tmdb.org/t/p/w185';
 
-           $.ajax({
-               url: apiBaseUrl + '/search/movie',
-               data: {
-                       api_key: '7780c6a90a66dfa3e0b69e733e848d17',
-                       query: searchText,
-                       language: 'it-IT'
-                     },
-               method: 'GET',
-               success: function (data) {
+            function creaCard() {
+               $.ajax({
+                   url: apiBaseUrl + '/search/movie',
+                   data: {
+                           api_key: '7780c6a90a66dfa3e0b69e733e848d17',
+                           query: searchText,
+                           language: 'it-IT'
+                         },
+                   method: 'GET',
+                   success: function (data) {
 
-                   var films = data.results;
-                   console.log(films);
-                   for (var i = 0; i < films.length; i++) {
-                       var film = films[i];
+                       var films = data.results;
 
-                       var stars = '';
-                       var stellaPiena = '<i class="fas fa-star"></i>';
-                       var stellaVuota = '<i class="far fa-star"></i>';
-                       var nStelle = Math.ceil(film.vote_average/2);
-                       for (var x = 0; x < 5; x++) {
+                       for (var i = 0; i < films.length; i++) {
+                           var film = films[i];
+                           var nStelle = Math.ceil(film.vote_average/2);
+                           var votiStelle = valutazioneStelle(nStelle);
 
-                           if(x < nStelle){
-                               stars = stars + stellaPiena;
+                           var datiFilm = {
+                               cover: imgBaseUrl + film.poster_path,
+                               titolo: film.title,
+                               titoloOr: film.original_title,
+                               trama: film.overview,
+                               rate: nStelle,
+                               linguaOr:film.original_language,
+                               stelle: votiStelle
                            }
-                           else {
-                               stars = stars + stellaVuota;
+                           if (datiFilm.linguaOr == 'en') {
+                               datiFilm.linguaOr = 'gb';
                            }
-                       }
 
-                       var datiFilm = {
-                           cover: imgBaseUrl + film.poster_path,
-                           titolo: film.title,
-                           titoloOr: film.original_title,
-                           trama: film.overview,
-                           rate: nStelle,
-                           linguaOr:film.original_language,
-                           stelle: stars
-                       }
-                       if (datiFilm.linguaOr == 'en') {
-                           datiFilm.linguaOr = 'gb';
-                       }
+                           var filmCard = template(datiFilm);
+                           $('.card-container').append(filmCard);
 
-                       var filmCard = template(datiFilm);
-                       $('.card-container').append(filmCard);
+                       }
+                   },
+                   error: function (err) {
 
                    }
-               },
-               error: function (err) {
+               })
+           };
 
-               }
-           })
+
+
+
+
+
+    function valutazioneStelle(numStelle) {
+        var stars = '';
+        var stellaPiena = '<i class="fas fa-star"></i>';
+        var stellaVuota = '<i class="far fa-star"></i>';
+
+        for (var x = 0; x < 5; x++) {
+
+            if(x < numStelle){
+                stars = stars + stellaPiena;
+            }
+            else {
+                stars = stars + stellaVuota;
+            }
+        }
+        return stars;
     };
-
-
 
 
 
