714c1f4004a3db318f58a2561ce7e88a18b405e7
Antonio <ameloni9@gmail.com>
2020-03-27 11:55:48 +0100

    Unite le due chiamate per film e serietv in un unica funzione

--- js/main.js
+++ js/main.js
@@ -4,15 +4,15 @@ $(document).ready(function () {
 
     $(".btn-search").click(function () {
         $('.card').remove();
-        creaFilmCard();
-        creaTVSeriesCard();
+        creaCard('/movie');
+        creaCard('/tv');
     });
 
     $('.search-input').keypress(function (event) {
         if (event.key == 'Enter') {
             $('.card').remove();
-            creaFilmCard();
-            creaTVSeriesCard();
+            creaCard('/movie',);
+            creaCard('/tv');
         }
     })
 
@@ -20,10 +20,10 @@ $(document).ready(function () {
             var apiBaseUrl = 'https://api.themoviedb.org/3';
             var imgBaseUrl = 'https://image.tmdb.org/t/p/w342';
 
-            function creaFilmCard() {
+            function creaCard(tipo, nome) {
                 var searchText = $(".search-input").val();
                $.ajax({
-                   url: apiBaseUrl + '/search/movie',
+                   url: apiBaseUrl + '/search' + tipo,
                    data: {
                            api_key: '7780c6a90a66dfa3e0b69e733e848d17',
                            query: searchText,
@@ -31,89 +31,57 @@ $(document).ready(function () {
                          },
                    method: 'GET',
                    success: function (data) {
-
                        var films = data.results;
-
-                       for (var i = 0; i < films.length; i++) {
-                           var film = films[i];
-                           var nStelle = Math.ceil(film.vote_average/2);
-                           var votiStelle = valutazioneStelle(nStelle);
-                          console.log(film);
-                           var datiFilm = {
-                               cover: imgBaseUrl + film.poster_path,
-                               coverError: '',
-                               titolo: film.title,
-                               titoloOr: film.original_title,
-                               trama: film.overview,
-                               rate: nStelle,
-                               linguaOr:film.original_language,
-                               stelle: votiStelle,
-                               trama: film.overview
-                           }
-                           if (datiFilm.linguaOr == 'en') {
-                               datiFilm.linguaOr = 'gb';
-                           }
-
-                           if (film.poster_path == null ) {
-                             datiFilm.coverError = 'Copertina in aggiornamento';
-                           }
-
-                           var filmCard = template(datiFilm);
-                           $('.card-container').append(filmCard);
-
-                       }
+                       stampaCards(films, tipo);
                    },
                    error: function (err) {
 
                    }
-               })
-           };
-
-            function creaTVSeriesCard() {
-                var searchText = $(".search-input").val();
-               $.ajax({
-                   url: apiBaseUrl + '/search/tv',
-                   data: {
-                           api_key: '7780c6a90a66dfa3e0b69e733e848d17',
-                           query: searchText,
-                           language: 'it-IT'
-                         },
-                   method: 'GET',
-                   success: function (data) {
-
-                       var films = data.results;
-
-                       for (var i = 0; i < films.length; i++) {
-                           var film = films[i];
-                           var nStelle = Math.ceil(film.vote_average/2);
-                           var votiStelle = valutazioneStelle(nStelle);
-
-                           var datiFilm = {
-                               cover: imgBaseUrl + film.poster_path,
-                               titolo: film.name,
-                               titoloOr: film.original_name,
-                               trama: film.overview,
-                               rate: nStelle,
-                               linguaOr:film.original_language,
-                               stelle: votiStelle
-                           }
-                           if (datiFilm.linguaOr == 'en') {
-                               datiFilm.linguaOr = 'gb';
-                           }
-
-                           var filmCard = template(datiFilm);
-                           $('.card-container').append(filmCard);
-
-                       }
-                   },
-                   error: function (err) {
-
-                   }
-               })
+               });
            };
 
 
+    function stampaCards(films, tipo) {
+      for (var i = 0; i < films.length; i++) {
+          var film = films[i];
+          var nStelle = Math.ceil(film.vote_average/2);
+          var votiStelle = valutazioneStelle(nStelle);
+         console.log(film);
+         var titolo, titoloOriginale;
 
+         if (tipo == '/movie') {
+           titolo = film.title;
+           titoloOriginale = film.original_title;
+         }
+
+         if (tipo == '/tv') {
+           titolo = film.name;
+           titoloOriginale = film.original_name;
+         }
+
+          var datiFilm = {
+              cover: imgBaseUrl + film.poster_path,
+              titolo: titolo,
+              titoloOr: titoloOriginale,
+              trama: film.overview,
+              rate: nStelle,
+              linguaOr:film.original_language,
+              stelle: votiStelle,
+              trama: film.overview
+          }
+          if (datiFilm.linguaOr == 'en') {
+              datiFilm.linguaOr = 'gb';
+          }
+
+          if (film.poster_path == null ) {
+            datiFilm.cover = 'https://www.2queue.com/2queue/wp-content/uploads/sites/6/tdomf/4299/movie-poster-coming-soon.png';
+          }
+
+          var filmCard = template(datiFilm);
+          $('.card-container').append(filmCard);
+
+      }
+    }
 
 
 
@@ -133,6 +101,7 @@ $(document).ready(function () {
         }
         return stars;
     };
+
 
 
 
